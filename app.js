dc94d9afaccc0162a32b1c9390919e8fa44cfad7
André Neves Xavier <andrenevesxavier276@gmail.com>
2025-08-04 20:47:47 -0300

    mudança teste

--- app.js
+++ app.js
@@ -1,70 +1,73 @@
-let quantidadeDeNumerosQuePodemSerSorteados = 4
-let numerosecreto = gerarNumeroAletorio();
-let listaNumeroSorteado = [];
-//função para editar o texto na tela
-function exibirTexto(tag, poema){
+let listaDeNumerosSorteados = [];
+let numeroLimite = 100;
+let numeroSecreto = gerarNumeroAleatorio();
+let tentativas = 1;
+
+function exibirTextoNaTela(tag, texto) {
     let campo = document.querySelector(tag);
-    campo.innerHTML = poema;
-    responsiveVoice.speak(poema, 'Brazilian Portuguese Female', {rate:1.2});
+    campo.innerHTML = texto;
+    responsiveVoice.speak(texto, 'Brazilian Portuguese Female', {rate:1.2});
 }
-function mensagemInicial() {
-    exibirTexto('h1', 'Jogo do número secreto');//mensagem que ira ficar no lugar do parametro tag e poema
-    exibirTexto('p' , `Digite um número entre 1 e ${quantidadeDeNumerosQuePodemSerSorteados}`);//mensagem que ira ficar no lugar do parametro tag e poema
-}
-mensagemInicial();
 
-let tentativa = 1;
-//função para verificar o chute do usuario
+function exibirMensagemInicial() {
+    exibirTextoNaTela('h1', 'Jogo do número secreto');
+    exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
+}
+
+exibirMensagemInicial();
+
 function verificarChute() {
-    let chute = document.querySelector('input') . value; //variavel onde sera guardado o chute do usuario
-           if (chute == ""){//condição caso o usuario aperte o botao chutar sem um valor
-            exibirTexto('p', 'Por favor, digite um número antes de chutar.');
-        }else{
-            if (chute == numerosecreto){// verificando se o usuario acertou o numero secreto
-                exibirTexto ('h1' , 'Você acertou');
-                let palavratentativa = tentativa == 1 ? 'tentativa' : 'tentativas';
-                let mensagemtentativa = (`voce descobriu o numero secreto com ${tentativa} ${palavratentativa}`);
-                exibirTexto ('p' , mensagemtentativa );  
-                document.getElementById ('reiniciar') . removeAttribute('disabled');//habilitando o botao novo jogo assim que o usuario acertar o numero secreto
-            }else {
-                if (chute > numerosecreto){
-                    exibirTexto ('p' , 'O numero secreto é menor');
-                }else{
-                     exibirTexto('p' , 'o numero secreto é maior');
-                }
-            tentativa ++;
-            LimparCampo ()
-            }           
-        }   
+    let chute = document.querySelector('input').value;
+    
+    if (chute == numeroSecreto) {
+        exibirTextoNaTela('h1', 'Acertou!');
+        let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa';
+        let mensagemTentativas = `Você descobriu o número secreto com ${tentativas} ${palavraTentativa}!`;
+        exibirTextoNaTela('p', mensagemTentativas);
+        document.getElementById('reiniciar').removeAttribute('disabled');
+    } else {
+        if (chute > numeroSecreto) {
+            exibirTextoNaTela('p', 'O número secreto é menor');
+        } else {
+            exibirTextoNaTela('p', 'O número secreto é maior');
+        }
+        tentativas++;
+        limparCampo();
+    }
 }
 
-//função para que quando o usuario errar o numero secreto, o campo onde digita o numero ficar vazio para digitar o proximo numero
-function LimparCampo() {
+function gerarNumeroAleatorio() {
+    let numeroEscolhido = parseInt(Math.random() * numeroLimite + 1);
+    let quantidadeDeElementosNaLista = listaDeNumerosSorteados.length;
+
+    if (quantidadeDeElementosNaLista == numeroLimite) {
+        listaDeNumerosSorteados = [];
+    }
+    if (listaDeNumerosSorteados.includes(numeroEscolhido)) {
+        return gerarNumeroAleatorio();
+    } else {
+        listaDeNumerosSorteados.push(numeroEscolhido);
+        console.log(listaDeNumerosSorteados)
+        return numeroEscolhido;
+    }
+}
+
+function limparCampo() {
     chute = document.querySelector('input');
     chute.value = '';
 }
-  
-//função para gerar um numero aleatorio
-function gerarNumeroAletorio() {
-    let numeroSorteado = parseInt (Math.random() * quantidadeDeNumerosQuePodemSerSorteados + 1);
-    let quantidadeDeElementoNaLista = listaNumeroSorteado.length;
-    if (quantidadeDeElementoNaLista == quantidadeDeNumerosQuePodemSerSorteados){
-        listaNumeroSorteado = [];
-    }
-    if (listaNumeroSorteado.includes(numeroSorteado)){
-        return gerarNumeroAletorio()
-    }else
-        listaNumeroSorteado.push(numeroSorteado);//numero sorteado sera colocado dentro da lista 
-        console.log(listaNumeroSorteado);
-        return numeroSorteado;
-        }
 
 function reiniciarJogo() {
-    numerosecreto = gerarNumeroAletorio();
-    LimparCampo();
-    tentativa = 1;
-    mensagemInicial();
-    document.getElementById ('reiniciar'). setAttribute('disabled', true);
+    numeroSecreto = gerarNumeroAleatorio();
+    limparCampo();
+    tentativas = 1;
+    exibirMensagemInicial();
+    document.getElementById('reiniciar').setAttribute('disabled', true)
 }
 
-let pintogrande = 20
+
+
+
+
+
+
